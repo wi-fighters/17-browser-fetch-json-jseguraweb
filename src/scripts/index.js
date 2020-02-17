d67ef5e4561d83dc7f1c32d68d9d58eea5e71d7c
Jaime Segura <jseguraweb@gmail.com>
2020-02-17 23:38:13 +0100

    create all cards with a promise

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -7,7 +7,7 @@ import { recipes } from './data.js';
 
 // 1. GET SECTIONS
 const cakesSection = document.querySelector('#cakes');
-const biscuitsSection = document.querySelector('#biscuit');
+const biscuitsSection = document.querySelector('#biscuits');
 const breadSection = document.querySelector('#bread');
 
 // 2. CREATE CARD
@@ -49,7 +49,7 @@ promiseOfRecipes.then(recipes => {
 })
 .then(list => {
     let cakes = list.cakes;
-    console.log(cakes);
+    // console.log(cakes);
     cakes.map(cake => {
         let card = document.createElement('div');
         card.classList.add('card');
@@ -74,8 +74,75 @@ promiseOfRecipes.then(recipes => {
         infoContainer.appendChild(title);
         infoContainer.appendChild(author);
         infoContainer.appendChild(ingredients);
-
+        // append picture and info containers in the card
         card.appendChild(picture);
         card.appendChild(infoContainer);
     })
+    return list;
+})
+.then(list => {
+    let biscuits = list.biscuits;
+    // console.log(biscuits);
+    biscuits.map(biscuit => {
+        let card = document.createElement('div');
+        card.classList.add('card');
+        biscuitsSection.appendChild(card);
+        // picture
+        let picture = document.createElement('img');
+        picture.classList.add('card-img');
+        picture.setAttribute('src', biscuit.image)
+        // info
+        const infoContainer = document.createElement('div');
+        infoContainer.classList.add('card-info');
+        // title of the card
+        const title = document.createElement('h3');
+        title.appendChild(document.createTextNode(biscuit.title));
+        // author
+        const author = document.createElement('p');
+        author.appendChild(document.createTextNode(biscuit.author));
+        // ingredients
+        const ingredients = document.createElement('p');
+        ingredients.appendChild(document.createTextNode(`Ingredients: ${biscuit.ingredients}`));
+        // append parts of the info in it
+        infoContainer.appendChild(title);
+        infoContainer.appendChild(author);
+        infoContainer.appendChild(ingredients);
+        // append picture and info containers in the card
+        card.appendChild(picture);
+        card.appendChild(infoContainer);
+    })
+    return list;
+})
+.then(list => {
+    let breads = list.bread;
+    // console.log(breads);
+    breads.map(bread => {
+        let card = document.createElement('div');
+        card.classList.add('card');
+        breadSection.appendChild(card);
+        // picture
+        let picture = document.createElement('img');
+        picture.classList.add('card-img');
+        picture.setAttribute('src', bread.image)
+        // info
+        const infoContainer = document.createElement('div');
+        infoContainer.classList.add('card-info');
+        // title of the card
+        const title = document.createElement('h3');
+        title.appendChild(document.createTextNode(bread.title));
+        // author
+        const author = document.createElement('p');
+        author.appendChild(document.createTextNode(bread.author));
+        // ingredients
+        const ingredients = document.createElement('p');
+        ingredients.appendChild(document.createTextNode(`Ingredients: ${bread.ingredients}`));
+        // append parts of the info in it
+        infoContainer.appendChild(title);
+        infoContainer.appendChild(author);
+        infoContainer.appendChild(ingredients);
+        // append picture and info containers in the card
+        card.appendChild(picture);
+        card.appendChild(infoContainer);
+    })
+    return list;
 })
