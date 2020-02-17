628b7b3ea8bdc4b92453a37a56e764095f32ab9b
Jaime Segura <jseguraweb@gmail.com>
2020-02-18 00:05:01 +0100

    refactor code to make it shorter

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -21,101 +21,48 @@ promiseOfRecipes.then(recipes => {
     return listOfRecipes;
 })
 .then(list => {
-    let cakes = list.cakes;
-    // console.log(cakes);
-    cakes.map(cake => {
-        let card = document.createElement('div');
-        card.classList.add('card');
-        cakesSection.appendChild(card);
-        // picture
-        let picture = document.createElement('img');
-        picture.classList.add('card-img');
-        picture.setAttribute('src', cake.image)
-        // info
-        const infoContainer = document.createElement('div');
-        infoContainer.classList.add('card-info');
-        // title of the card
-        const title = document.createElement('h3');
-        title.appendChild(document.createTextNode(cake.title));
-        // author
-        const author = document.createElement('p');
-        author.appendChild(document.createTextNode(cake.author));
-        // ingredients
-        const ingredients = document.createElement('p');
-        ingredients.appendChild(document.createTextNode(`Ingredients: ${cake.ingredients}`));
-        // append parts of the info in it
-        infoContainer.appendChild(title);
-        infoContainer.appendChild(author);
-        infoContainer.appendChild(ingredients);
-        // append picture and info containers in the card
-        card.appendChild(picture);
-        card.appendChild(infoContainer);
-    })
-    return list;
-})
-.then(list => {
-    let biscuits = list.biscuits;
-    // console.log(biscuits);
-    biscuits.map(biscuit => {
-        let card = document.createElement('div');
-        card.classList.add('card');
-        biscuitsSection.appendChild(card);
-        // picture
-        let picture = document.createElement('img');
-        picture.classList.add('card-img');
-        picture.setAttribute('src', biscuit.image)
-        // info
-        const infoContainer = document.createElement('div');
-        infoContainer.classList.add('card-info');
-        // title of the card
-        const title = document.createElement('h3');
-        title.appendChild(document.createTextNode(biscuit.title));
-        // author
-        const author = document.createElement('p');
-        author.appendChild(document.createTextNode(biscuit.author));
-        // ingredients
-        const ingredients = document.createElement('p');
-        ingredients.appendChild(document.createTextNode(`Ingredients: ${biscuit.ingredients}`));
-        // append parts of the info in it
-        infoContainer.appendChild(title);
-        infoContainer.appendChild(author);
-        infoContainer.appendChild(ingredients);
-        // append picture and info containers in the card
-        card.appendChild(picture);
-        card.appendChild(infoContainer);
-    })
-    return list;
-})
-.then(list => {
-    let breads = list.bread;
-    // console.log(breads);
-    breads.map(bread => {
-        let card = document.createElement('div');
-        card.classList.add('card');
-        breadSection.appendChild(card);
-        // picture
-        let picture = document.createElement('img');
-        picture.classList.add('card-img');
-        picture.setAttribute('src', bread.image)
-        // info
-        const infoContainer = document.createElement('div');
-        infoContainer.classList.add('card-info');
-        // title of the card
-        const title = document.createElement('h3');
-        title.appendChild(document.createTextNode(bread.title));
-        // author
-        const author = document.createElement('p');
-        author.appendChild(document.createTextNode(bread.author));
-        // ingredients
-        const ingredients = document.createElement('p');
-        ingredients.appendChild(document.createTextNode(`Ingredients: ${bread.ingredients}`));
-        // append parts of the info in it
-        infoContainer.appendChild(title);
-        infoContainer.appendChild(author);
-        infoContainer.appendChild(ingredients);
-        // append picture and info containers in the card
-        card.appendChild(picture);
-        card.appendChild(infoContainer);
-    })
+    for (let recipe in list) {
+        let food = list[recipe];
+
+        food.map(typeOfFood => {
+            // console.log(typeOfFood.type);
+            
+            // create card element
+            let card = document.createElement('div');
+            card.classList.add('card');
+            // picture
+            let picture = document.createElement('img');
+            picture.classList.add('card-img');
+            picture.setAttribute('src', typeOfFood.image);
+            // info
+            const infoContainer = document.createElement('div');
+            infoContainer.classList.add('card-info');
+            // title of the card
+            const title = document.createElement('h3');
+            title.appendChild(document.createTextNode(typeOfFood.title));
+            // author
+            const author = document.createElement('p');
+            author.appendChild(document.createTextNode(typeOfFood.author));
+            // ingredients
+            const ingredients = document.createElement('p');
+            ingredients.appendChild(document.createTextNode(`Ingredients: ${typeOfFood.ingredients}`));
+            // append parts of the info in the info container
+            infoContainer.appendChild(title);
+            infoContainer.appendChild(author);
+            infoContainer.appendChild(ingredients);
+            // append picture and info containers in the card element
+            card.appendChild(picture);
+            card.appendChild(infoContainer);
+
+            if (typeOfFood.type === 'cakes') {
+                cakesSection.appendChild(card);
+
+            } else if (typeOfFood.type === 'biscuits') {
+                biscuitsSection.appendChild(card);
+            } else if (typeOfFood.type === 'bread') {
+                breadSection.appendChild(card);
+            }
+        })
+    }
     return list;
 })
