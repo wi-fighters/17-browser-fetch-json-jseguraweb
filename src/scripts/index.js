e6b9e8828245f2db9ac4bb98e7023ad68c623c0c
Jaime Segura <jseguraweb@gmail.com>
2020-02-17 23:39:57 +0100

    clean code

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,39 +5,12 @@ import { recipes } from './data.js';
 
 // \/ All of your javascript should go here \/
 
-// 1. GET SECTIONS
+// GET SECTIONS
 const cakesSection = document.querySelector('#cakes');
 const biscuitsSection = document.querySelector('#biscuits');
 const breadSection = document.querySelector('#bread');
 
-// 2. CREATE CARD
-const card = document.createElement('div');
-card.classList.add('card');
-// picture
-const picture = document.createElement('img');
-picture.classList.add('card-img');
-// info
-const infoContainer = document.createElement('div');
-infoContainer.classList.add('card-info');
-// title of the card
-const title = document.createElement('h3');
-title.appendChild(document.createTextNode('title'));
-// author
-const author = document.createElement('p');
-author.appendChild(document.createTextNode('author'));
-// ingredients
-const ingredients = document.createElement('p');
-ingredients.appendChild(document.createTextNode('ingredients'));
-// append parts of the info in it
-infoContainer.appendChild(title);
-infoContainer.appendChild(author);
-infoContainer.appendChild(ingredients);
-// append picture and info containers in the card
-card.appendChild(picture);
-card.appendChild(infoContainer);
-// cakesSection.appendChild(card)
-
-// 3. CREATE PROGRAM
+// CREATE PROGRAM
 let promiseOfRecipes = new Promise((resolve, reject) => {
     resolve(recipes)
 })
