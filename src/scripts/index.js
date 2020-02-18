9c6d7726f0ebcd631d39ee68200fb807861bf62e
Jaime Segura <jseguraweb@gmail.com>
2020-02-18 20:30:53 +0100

    solve exercise without promise

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -11,58 +11,47 @@ const biscuitsSection = document.querySelector('#biscuits');
 const breadSection = document.querySelector('#bread');
 
 // CREATE PROGRAM
-let promiseOfRecipes = new Promise((resolve, reject) => {
-    resolve(convertedData);
-})
 
-promiseOfRecipes.then(recipes => {
-    let listOfRecipes = JSON.parse(recipes);
-    // console.log(listOfRecipes);
-    return listOfRecipes;
-})
-.then(list => {
-    for (let recipe in list) {
-        let food = list[recipe];
+let recipes = JSON.parse(convertedData);
+// console.log(recipes);    
 
-        food.map(typeOfFood => {
-            // console.log(typeOfFood.type);
-            
-            // create card element
-            let card = document.createElement('div');
-            card.classList.add('card');
-            // picture
-            let picture = document.createElement('img');
-            picture.classList.add('card-img');
-            picture.setAttribute('src', typeOfFood.image);
-            // info
-            const infoContainer = document.createElement('div');
-            infoContainer.classList.add('card-info');
-            // title of the card
-            const title = document.createElement('h3');
-            title.appendChild(document.createTextNode(typeOfFood.title));
-            // author
-            const author = document.createElement('p');
-            author.appendChild(document.createTextNode(typeOfFood.author));
-            // ingredients
-            const ingredients = document.createElement('p');
-            ingredients.appendChild(document.createTextNode(`Ingredients: ${typeOfFood.ingredients}`));
-            // append parts of the info in the info container
-            infoContainer.appendChild(title);
-            infoContainer.appendChild(author);
-            infoContainer.appendChild(ingredients);
-            // append picture and info containers in the card element
-            card.appendChild(picture);
-            card.appendChild(infoContainer);
+for (let recipe in recipes) {
+    let desert = recipes[recipe];
 
-            if (typeOfFood.type === 'cakes') {
-                cakesSection.appendChild(card);
+    desert.map(typeOfFood => {
+        // create card element
+        let card = document.createElement('div');
+        card.classList.add('card');
+        // picture
+        let picture = document.createElement('img');
+        picture.classList.add('card-img');
+        picture.setAttribute('src', typeOfFood.image);
+        // info
+        const infoContainer = document.createElement('div');
+        infoContainer.classList.add('card-info');
+        // title of the card
+        const title = document.createElement('h3');
+        title.appendChild(document.createTextNode(typeOfFood.title));
+        // author
+        const author = document.createElement('p');
+        author.appendChild(document.createTextNode(typeOfFood.author));
+        // ingredients
+        const ingredients = document.createElement('p');
+        ingredients.appendChild(document.createTextNode(`Ingredients: ${typeOfFood.ingredients}`));
+        // append parts of the info in the info container
+        infoContainer.appendChild(title);
+        infoContainer.appendChild(author);
+        infoContainer.appendChild(ingredients);
+        // append picture and info containers in the card element
+        card.appendChild(picture);
+        card.appendChild(infoContainer);
 
-            } else if (typeOfFood.type === 'biscuits') {
-                biscuitsSection.appendChild(card);
-            } else if (typeOfFood.type === 'bread') {
-                breadSection.appendChild(card);
-            }
-        })
-    }
-    return list;
-})
+        if (typeOfFood.type === 'cakes') {
+            cakesSection.appendChild(card);
+        } else if (typeOfFood.type === 'biscuits') {
+            biscuitsSection.appendChild(card);
+        } else if (typeOfFood.type === 'bread') {
+            breadSection.appendChild(card);
+        }
+    })
+}
